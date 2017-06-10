3b12bea3a23f2ad7c19c68249a968e50ffb254d4
Svetoslav Slavov <svetoslav_slavov@icloud.com>
2017-06-10 22:10:56 +0300

    First Commit

---
+++ MergeIntervals/GetMaxSubArrtSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeIntervals
+{
+    public class GetMaxSubArrtSum
+    {
+        public static int GetMaxSubArrySum(int[] array)
+        {
+            int size = array.Length;
+            if (array.Length == 0)
+                throw new Exception("empty array");
+            int curr = 0;
+            int sum = 0;
+            int max = 0;
+            while (curr < size)
+            {
+                sum += array[curr++];
+                if (sum > max)
+                    max = sum;
+                else if (sum < 0)
+                    sum = 0;
+            }
+            return max;
+        }
+    }
+}
---
+++ MergeIntervals/StackMin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeIntervals
+{
+    public class StackMin
+    {
+        struct MinStackElement
+        {
+            public int data;
+            public int min;
+        }
+
+        int top = 0;
+        List<MinStackElement> _stackMin = new List<MinStackElement>();
+
+        public void Push(int data)
+        {
+            if (top == 0)
+            {
+                _stackMin.Add(new MinStackElement() { data = data, min = data });
+            }
+            else
+            {
+                var stackElement = new MinStackElement() { data = data, min = _stackMin[top - 1].min };
+                if (stackElement.min > data)
+                    stackElement.min = data;
+                _stackMin.Add(stackElement);
+            }
+            top++;
+        }
+        public int Pop()
+        {
+            int data = _stackMin[top - 1].data;
+            _stackMin.RemoveAt(top - 1);
+            top--;
+            return data;
+        }
+        public int GetMin()
+        {
+            if (top == 0)
+                throw new Exception("Stack is empty.");
+            var min = _stackMin[top - 1].min;
+            return min;
+        }
+    }
+}
